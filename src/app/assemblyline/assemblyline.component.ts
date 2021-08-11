1a326d91083365a2b8890e2a1667a6a3fee27827
Shashankshekhar22 <itsshashankshekhar@gmail.com>
2021-08-11 18:35:18 +0530

    Implementation of Assembly line code

--- src/app/assemblyline/assemblyline.component.ts
+++ src/app/assemblyline/assemblyline.component.ts
@@ -1,11 +1,45 @@
-import { Component, Input } from "@angular/core";
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: "assemblyline",
-  templateUrl: "./assemblyline.component.html",
-  styleUrls: ["./assemblyline.component.scss"]
+  selector: 'assemblyline',
+  templateUrl: './assemblyline.component.html',
+  styleUrls: ['./assemblyline.component.scss']
 })
-export class AssemblyLineComponent {
+export class AssemblyLineComponent implements OnInit {
   @Input() stages: string[];
+  listItems = {};
 
+  ngOnInit(): void {
+    this.stages.forEach((stage: string) => {
+      this.listItems[stage] = [];
+    });
+  }
+
+  onEnterKeyPressed(event: any): void {
+    const stageOne = this.stages[0];
+    if (event.target && event.target.value) {
+      this.listItems[stageOne].unshift(event.target.value);
+    }
+  }
+
+  onContextMenuClick(event: any, item: string, stage: string, stageIndex: number, itemIndex: number): void {
+    if (stageIndex === 0) {
+      this.listItems[stage].splice(itemIndex, 1);
+    } else {
+      const prevStage = this.stages[stageIndex - 1];
+      this.listItems[prevStage].push(item);
+      this.listItems[stage].splice(itemIndex, 1);
+    }
+    event.preventDefault();
+  }
+
+  onLeftClick(item: string, stage: string, stageIndex: number, itemIndex: number): void {
+    if (stageIndex === 3) {
+      this.listItems[stage].splice(itemIndex, 1);
+    } else {
+      const nextStage = this.stages[stageIndex + 1];
+      this.listItems[nextStage].unshift(item);
+      this.listItems[stage].splice(itemIndex, 1);
+    }
+  }
 }
